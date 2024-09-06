e7a653b00e1b94118152cd1e02f3ee1acaded68d
Zahra <zahra.hasan@bjss>
2024-09-06 09:31:24 +0100

    teamName value changed

--- frontend/src/appconfig.js
+++ frontend/src/appconfig.js
@@ -1,7 +1,7 @@
 const config = {
   imageServiceBaseUrl:
     "https://bjssacademyhackday-a8fqfwapbxfpa4b5.uksouth-01.azurewebsites.net",
-  teamName: "example",
+  teamName: "commodore",
 
   imageServiceUrl: function () {
     return `${config.imageServiceBaseUrl}/IL/teams/${config.teamName}/files`;
